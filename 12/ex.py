e4a6b6e39f76b8a475636a3b3854a19ba93fd4d0
Priyanka Mondal <pmondal@udel.edu>
2020-10-03 12:27:18 -0400

    Strong password and place a flower

---
+++ 12/ex.py
@@ -0,0 +1,89 @@
+from itertools import groupby
+import numpy.random as rnd
+
+class Solution:
+  def strongpassword(self, s):
+    """
+      s : inpit stirng
+    """
+    # It has at least 6 characters and at most 20 characters.
+    def charlen(s):
+      """
+      Returns True if string has character min char
+      of 6 and max char of 20
+      """
+      # print("len char : ", len(s))
+      if len(s) >= 6 and len(s) <= 20 :
+        return True
+      else:
+        print("Password must be atleast 6 char")
+        return False
+
+    def mod_charlen(s):
+        # Add random integer between 0-9 to make it six character
+        # An alternate would be to pick a random char from the string and add
+        # in the end
+
+        for ii in range(6-len(s)):
+          s = s + str(rnd.randint(0, 9, size=1)[0])
+        print("suggested password : ", s)
+        return s
+
+    def chkletter(s):
+      """
+      It must contain at least one lowercase letter, 
+      at least one uppercase letter, and at least one digit
+      """
+      # Check if any char is lower case
+      lw_wrd = any(l.islower() for l in s)
+
+      # Check if any char is upper case
+      up_wrd = any(u.isupper() for u in s)
+
+      # Check if any char is digit
+      dig = any(d.isdigit() for d in s)
+     
+      # Return true if all above conditions are satisfied
+      if lw_wrd & up_wrd & dig:
+        return True
+      else:
+        return False
+       
+       
+    def chkrepeat(s):
+      """
+      It must NOT contain three repeating characters in a row
+      Note : The function can differentiate between upper and lower cases
+      """
+      groups = groupby(s)
+      result = [(label, sum(1 for _ in group)) for label, group in groups]
+      repeat_char_len = [result[ii][1] for ii in range(len(result))]
+      repeat_char_pos = any(i-2>0 for i in repeat_char_len)
+      # print(repeat_char_pos)
+      return repeat_char_pos
+
+    def strngInptPsswrd(s):
+      """
+      returns 0 if all strong password requirements are met 
+      """
+      
+      if charlen(s): 
+           if chkletter(s) and not chkrepeat(s):
+              # TODO
+              # modify_chkletter 
+              # modify_chkrepeat
+              return 0
+      else:
+           return mod_charlen(s)
+
+
+    return strngInptPsswrd(s)
+
+# Test example
+# Works for strong password check
+# TODO
+# Need to work on modification part 
+string = "Gr2s61"# "grrree2Ss"
+sol = Solution().strongpassword(string)
+print(sol)
+    
---
+++ 13/ex.py
@@ -0,0 +1,47 @@
+class Solution:
+
+  def flowerbed(self, lst, n):
+      """
+      Find an empty spot and check if the site before 
+      and after are empty and count such spots
+      """
+      empty = 0
+      empty_detec = 0 
+      for it, item in enumerate(lst):
+         # Find an empty spot and check if spots on left and 
+         # right are empty
+         
+         # Considering only sites from 1 to N-1
+         if empty_detec ==1:
+            empty_detec = 0 
+            # If an empty spot was detected in previous spot pass
+            continue
+         print(it) 
+         if it > 0 and it < len(lst)-1:
+            if item == 0:
+               if lst[it-1] == 0 and lst[it+1] == 0:
+                  empty +=1
+                  empty_detec = 1
+
+         # Check for first and last site availability
+         if it == 0:
+            if item == 0 and len(lst) >=2:
+               if lst[it+1] == 0:
+                  empty +=1
+                  empty_detec = 1
+         if it == len(lst)-1:
+            if item == 0:
+               if lst[it -1] == 0:
+                  empty +=1
+                  empty_detec = 1
+      return ( n <= empty )
+
+# lst = [1,0,0,0,1]
+# lst = [1,0,0,0,0,1]
+# lst = [1,0,0,0,1,0,0]
+# lst = [0, 0, 0]
+lst = [0, 0]
+
+n = 1
+sol = Solution()
+print sol.flowerbed(lst, n)
